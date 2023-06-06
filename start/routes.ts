ad84a9a33ed052c6c0ab27489717b285eb8fd7d6
MuhammadUmarMalik <malikgraphics811@gmail.com>
2023-06-06 21:41:22 +0500

    do migration and create schema and change in route

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,9 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.get('/user', async () => {
+  return {
+    umar: 'Malik',
+  }
+})
